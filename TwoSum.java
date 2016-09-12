9a55aa321320ee3dc9c71d3e12ab954658b561bf
Chen Xia <372340958@QQ.COM>
2016-09-11 23:07:23 -0500

    Add files via upload

---
+++ TwoSum.java
@@ -0,0 +1,35 @@
+/* This question if from:
+ * https://leetcode.com/problems/two-sum/
+ * Time complexity : O(n)
+ * */
+
+import java.util.HashMap;
+
+public class TwoSum {
+	
+	public static void main( String []args ) {
+		// Define an array and display the result array on console
+		int[] nums = {0,4,3,-3};
+		int[] ret = new int[2];
+
+		TwoSum t = new TwoSum();
+		ret = t.twoSum( nums,2 );
+		for( int i=0; i < ret.length; i++)
+			System.out.println( ret[i] );
+	}
+	public int[] twoSum(int[] nums, int target) {
+		int i;
+		HashMap<Integer,Integer> map = new HashMap<>();
+		// Store all elements into a hash map
+		for( i=0; i<nums.length; i++ ) {
+			map.put(nums[i],i);
+		}
+		for( i = 0; i<nums.length; i++ ){
+			int difference = target - nums[i];
+			if( map.containsKey(difference) && map.get(difference) != i ){
+				return new int[] { i, map.get(difference) };
+			}
+		}
+		throw new IllegalArgumentException("No solution");
+	}
+}
