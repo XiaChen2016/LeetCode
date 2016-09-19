6e56ca7dd985a579687216b5b2adb7dc5adc6748
Chen Xia <372340958@QQ.COM>
2016-09-19 13:43:57 -0500

    Add files via upload

---
+++ PlusOne.java
@@ -0,0 +1,29 @@
+/* The source of question:
+ * https://leetcode.com/problems/plus-one/
+ * */
+public class PlusOne {
+	
+	public static void main( String[] args ) {
+		PlusOne p = new PlusOne();
+		// Define a number represented as an array of digits 
+		int[] digits = {1,2,3,1,2};
+		// Get the new number
+		int[] array = p.plusOne(digits);
+		for( int i=0; i< array.length; i++ )
+			System.out.print(array[i]);
+		
+	}
+    public int[] plusOne(int[] digits) {
+        int i;
+        for( i=digits.length-1 ; i>=0; i-- ) {
+        	if( digits[i] < 9 ) {
+        		digits[i] ++;
+        		return digits;
+        	}
+        	digits[i] = 0;
+        }
+        int[] array = new int[digits.length+1];
+        array[0] = 1;
+        return array;
+    }
+}
